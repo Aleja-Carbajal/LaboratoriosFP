4234a828d4303c191f1b6222688c31609b6a6835
Aleja-Carbajal <00253720@uca.edu.sv>
2020-05-20 17:49:36 -0600

    Laboratorio5

--- Segudo_par.cpp
+++ Segudo_par.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main (){
+
+    int num, div;
+    cout <<"Saber si un numero es par o impar"<<endl;
+    cout <<"Ingrese el numero: "<<endl;
+    cin >> num;
+    div = num%2;
+
+    if (div==0)
+    {
+        cout << "Si es par";
+    }
+    else
+    {
+        cout <<"No es par";
+    }
+    
+
+    
     
 }
