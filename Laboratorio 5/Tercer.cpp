8a8b5307546c132212c779529cf17225b7158865
Aleja-Carbajal <62610164+Aleja-Carbajal@users.noreply.github.com>
2020-05-20 18:35:08 -0600

    Add files via upload

---
+++ Laboratorio 5/Tercer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int va,res;
+    cout <<"Conocer si un numero es positivo, neativo o cero"<< endl;
+    cout << "Ingrese el numero: "<<endl;
+    cin >> va;
+    
+    if (/* condition */)
+    {
+        /* code */
+    }
+    else if ()
+    {
+        /* code */
+    }
+    else
+    {
+        /* code */
+    }
+    
+}
